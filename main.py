403378f6c2e693c2676e7e18523a19e272813f2d
opala82 <tomato.riko@gmail.com>
2019-05-19 17:42:27 +0200

    Added saving to file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import io
+
 import requests
 import json
 import re
@@ -5,10 +7,13 @@ from bs4 import BeautifulSoup
 
 url = "https://www.zurnal24.si"
 response = requests.get(url)
+data = []
 
 soup = BeautifulSoup(response.content, "html.parser")
 
+ct = 0
 for a in soup.find_all('a', href=True):
+    ct = ct + 1
     if a['href'].startswith('/') and re.search(r'\d+$', a['href']):
         suburl = url + a['href']
         sub_soup = BeautifulSoup(requests.get(suburl).content, "html.parser")
@@ -17,4 +22,12 @@ for a in soup.find_all('a', href=True):
         for author in sub_soup.find('div', {"class": "article__authors"}).find_all("a"):
             authors.append(author.text.strip())
         excerpt = sub_soup.find('div', {"class": "article__leadtext"}).text.strip()
-        print({title, ', '.join(authors), excerpt})
+        data.append((title, excerpt, authors))
+
+        if ct >= 5:
+            break
+
+with io.open("data.json", "w", encoding='utf8') as datafile:
+    json.dump(data, datafile, ensure_ascii=False)
+
+print("Done.")
