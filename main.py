ec2fd48c45e9744769add3dad1254a2ae7167fd1
opala82 <tomato.riko@gmail.com>
2019-05-19 17:02:19 +0200

    Changed link scraping

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 import urllib.request
 import time
+import re
 from bs4 import BeautifulSoup
 
-url = "https://www.zurnal24.si/"
+url = "https://www.zurnal24.si"
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
-a_tags = soup.findAll('a')
-print(a_tags)
+
+for a in soup.find_all('a', href=True):
+    if a['href'].startswith('/') and re.search(r'\d+$', a['href']):
+        print(url + a['href'])
