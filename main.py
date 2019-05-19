1c06043ac804031fbe1c736be2c8c8a44905d72d
opala82 <tomato.riko@gmail.com>
2019-05-19 17:24:57 +0200

    Fixed scraper

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import requests
-import urllib.request
-import time
+import json
 import re
 from bs4 import BeautifulSoup
 
 url = "https://www.zurnal24.si"
 response = requests.get(url)
 
-soup = BeautifulSoup(response.text, "html.parser")
+soup = BeautifulSoup(response.content, "html.parser")
 
 for a in soup.find_all('a', href=True):
     if a['href'].startswith('/') and re.search(r'\d+$', a['href']):
-        print(url + a['href'])
+        suburl = url + a['href']
+        sub_soup = BeautifulSoup(requests.get(suburl).content, "html.parser")
+        title = sub_soup.find('h1', {"class": "article__title"}).text
+        authors = []
+        for author in sub_soup.find('div', {"class": "article__authors"}).find_all("a"):
+            authors.append(author.text.strip())
+        excerpt = sub_soup.find('div', {"class": "article__leadtext"}).text.strip()
+        print({title, ', '.join(authors), excerpt})
