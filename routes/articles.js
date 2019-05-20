1516705d4ebbbf2e9e2242050d18ddae89cbe518
ilion227 <janliber1@gmail.com>
2019-05-20 23:29:48 +0200

    Added single page layout

--- routes/articles.js
+++ routes/articles.js
@@ -3,11 +3,10 @@ const Article = require('../models/Article');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', async function(req, res, next) {
 	const articles = await Article.find({}).sort({'meta.publishedAt': 1}).exec();
 
-	res.render('pages/articles', {layout: 'default', articles: articles});
+	res.render('pages/articles', {layout: 'single', articles});
 });
 
 router.get('/fetch', async function(req, res, next) {
@@ -15,6 +14,12 @@ router.get('/fetch', async function(req, res, next) {
 		if (err) return res.status(500).json({error: err});
 		res.json({articles});
 	});
+});
+
+router.get('/:id', async function(req, res, next) {
+	const article = await Article.findById(req.params.id).exec();
+
+	res.render('pages/article', {layout: 'single', article});
 });
 
 module.exports = router;
--- routes/websites.js
+++ routes/websites.js
@@ -7,7 +7,14 @@ var router = express.Router();
 router.get('/', async function(req, res, next) {
 	const websites = await Website.find({}).exec();
 
-	res.render('pages/websites', {layout: 'default', websites});
+	res.render('pages/websites', {layout: 'single', websites});
+});
+
+/* GET home page. */
+router.get('/:id', async function(req, res, next) {
+	const website = await Website.findById(req.params.id).exec();
+
+	res.render('pages/website', {layout: 'single', website});
 });
 
 module.exports = router;
