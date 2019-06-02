f645bce44baae7d6ea529a6f5bbcbc6de12fcc0b
ilion227 <janliber1@gmail.com>
2019-06-02 16:38:31 +0200

    Fixed ObjectId bug

--- external/generate_features.py
+++ external/generate_features.py
@@ -1,19 +1,24 @@
 import sys
-import pprint
+
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 if len(sys.argv) < 2:
     print("Article's ID from MongoDB must be provided!")
     sys.exit()
 
-article_id = sys.argv[1]
+article_id = ObjectId(sys.argv[1])
 
-print("ID of the article to process images: " + article_id)
+print("ID of the article to process images: " + str(article_id))
 
 client = MongoClient('mongodb://localhost:27017/')
 
 db = client["news-scraper"]
 
-
 articles_collection = db.articles
-pprint.pprint(articles_collection.find_one())
+
+article = articles_collection.find_one({"_id": article_id})
+images = article['images']
+
+for image in images:
+    print("Processing", images)
