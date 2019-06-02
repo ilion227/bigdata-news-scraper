f829c678d01ba3011b2f524f69d7d1b6fad9eb1c
ilion227 <janliber1@gmail.com>
2019-06-02 17:27:28 +0200

    Added code for other methods of feature generation.

--- external/generate_features.py
+++ external/generate_features.py
@@ -4,13 +4,245 @@ import urllib.request
 import cv2 as cv
 import math
 import numpy as np
+from bitstring import BitArray
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 CELL_SIZE = (16, 16)
 BLOCK_SIZE = (2, 2)
-RESIZE_DIMENSIONS = (128, 256)
+RESIZE_DIMENSIONS = (64, 128)
 DISTANCE = 2
+
+
+def calculate_lbp(img, shape):
+    img = cv.resize(img, RESIZE_DIMENSIONS)
+
+    new_img = img.copy()
+    height, width = img.shape
+    bins = 256
+
+    histograms = []
+    for y in range(0, height, CELL_SIZE[1]):
+        for x in range(0, width, CELL_SIZE[0]):
+
+            local_histogram = np.zeros(bins)
+            for j in range(y, y + CELL_SIZE[1]):
+                for i in range(x, x + CELL_SIZE[0]):
+                    try:
+                        center = img[j, i]
+
+                        values = [img[j - 1, i - 1], img[j, i - 1],
+                                  img[j + 1, i - 1], img[j + 1, i],
+                                  img[j + 1, i + 1],
+                                  img[j, i + 1], img[j - 1, i + 1],
+                                  img[j - 1, i]]
+
+                        binary = []
+                        for val in values:
+                            if val >= center:
+                                binary.append(1)
+                            else:
+                                binary.append(0)
+
+                        # Convert binary to integer value
+                        binary = np.array(binary)
+                        bit_array = BitArray(binary)
+                        result = bit_array.uint
+
+                        new_img[j, i] = result
+
+                        local_histogram[result] += 1
+                    except IndexError:
+                        pass
+
+            squared = np.square(local_histogram)
+            squared = squared + math.pow(0.0001, 2)
+            l2_norm = np.sum(squared)
+
+            normalized_local_histogram = local_histogram / l2_norm
+            histograms.append(normalized_local_histogram)
+
+    return {
+        "cell_dimensions": {
+            "x": CELL_SIZE[0],
+            "y": CELL_SIZE[1]
+        },
+        "block_dimensions": {
+            "x": BLOCK_SIZE[0],
+            "y": BLOCK_SIZE[1]
+        },
+        "n_bins": bins,
+        "img_dimensions": {
+            "x": shape[1],
+            "y": shape[0]
+        },
+        "resized_img_dimensions": {
+            "x": RESIZE_DIMENSIONS[0],
+            "y": RESIZE_DIMENSIONS[1]
+        },
+        "values": np.concatenate(histograms).ravel().tolist()
+    }
+
+
+def calculate_lbp_uniform(img, shape):
+    img = cv.resize(img, RESIZE_DIMENSIONS)
+
+    new_img = img.copy()
+    height, width = img.shape
+    bins = 59
+
+    histograms = []
+    for y in range(0, height, CELL_SIZE[1]):
+        for x in range(0, width, CELL_SIZE[0]):
+
+            local_histogram = np.zeros(bins)
+            for j in range(y, y + CELL_SIZE[1]):
+                for i in range(x, x + CELL_SIZE[0]):
+                    changes = 0
+                    try:
+                        center = img[j, i]
+
+                        values = [img[j - 1, i - 1], img[j, i - 1],
+                                  img[j + 1, i - 1], img[j + 1, i],
+                                  img[j + 1, i + 1],
+                                  img[j, i + 1], img[j - 1, i + 1],
+                                  img[j - 1, i]]
+
+                        last = []
+                        binary = []
+                        for val in values:
+                            if val >= center:
+                                binary.append(1)
+                            else:
+                                binary.append(0)
+                            last.append(binary[-1])
+                            if len(last) > 1 and last[-1] != last[-2]:
+                                changes = changes + 1
+
+                        # Convert binary to integer value
+                        binary = np.array(binary)
+                        bit_array = BitArray(binary)
+                        result = bit_array.uint
+                        if changes <= 2:
+                            local_histogram[result] += 1
+                            new_img[j, i] = result
+                        else:
+                            # Last bin is for non uniform values
+                            local_histogram[bins - 1] += 1
+                            new_img[j, i] = bins - 1
+
+                    except IndexError:
+                        pass
+
+            squared = np.square(local_histogram)
+            squared = squared + math.pow(0.0001, 2)
+            l2_norm = np.sum(squared)
+
+            normalized_local_histogram = local_histogram / l2_norm
+            histograms.append(normalized_local_histogram)
+
+    return {
+        "cell_dimensions": {
+            "x": CELL_SIZE[0],
+            "y": CELL_SIZE[1]
+        },
+        "block_dimensions": {
+            "x": BLOCK_SIZE[0],
+            "y": BLOCK_SIZE[1]
+        },
+        "n_bins": bins,
+        "img_dimensions": {
+            "x": shape[1],
+            "y": shape[0]
+        },
+        "resized_img_dimensions": {
+            "x": RESIZE_DIMENSIONS[0],
+            "y": RESIZE_DIMENSIONS[1]
+        },
+        "values": np.concatenate(histograms).ravel().tolist()
+    }
+
+
+def calculate_lbp_distance(img, shape, d):
+    img = cv.resize(img, RESIZE_DIMENSIONS)
+
+    new_img = img.copy()
+    height, width = img.shape
+    bins = 256
+
+    histograms = []
+    for y in range(0, height, CELL_SIZE[1]):
+        for x in range(0, width, CELL_SIZE[0]):
+            local_histogram = np.zeros(bins)
+            for j in range(y, y + CELL_SIZE[1]):
+                for i in range(x, x + CELL_SIZE[0]):
+                    next_index = d
+                    comparisons = 0
+
+                    binary = []
+
+                    try:
+                        while comparisons < 8:
+                            values = [img[j - 1, i - 1], img[j, i - 1],
+                                      img[j + 1, i - 1], img[j + 1, i],
+                                      img[j + 1, i + 1],
+                                      img[j, i + 1], img[j - 1, i + 1],
+                                      img[j - 1, i]]
+
+                            while next_index >= len(values):
+                                next_index -= len(values)
+                            center = values[next_index]
+
+                            val = values[comparisons]
+
+                            if val >= center:
+                                binary.append(1)
+                            else:
+                                binary.append(0)
+
+                            comparisons += 1
+                            next_index = comparisons + d
+
+                        # Convert binary to integer value
+                        binary = np.array(binary)
+                        bit_array = BitArray(binary)
+                        result = bit_array.uint
+
+                        new_img[j, i] = result
+
+                        local_histogram[result] += 1
+
+                    except IndexError:
+                        pass
+
+            squared = np.square(local_histogram)
+            squared = squared + math.pow(0.0001, 2)
+            l2_norm = np.sum(squared)
+
+            normalized_local_histogram = local_histogram / l2_norm
+            histograms.append(normalized_local_histogram)
+
+    return {
+        "distance": d,
+        "cell_dimensions": {
+            "x": CELL_SIZE[0],
+            "y": CELL_SIZE[1]
+        },
+        "block_dimensions": {
+            "x": BLOCK_SIZE[0],
+            "y": BLOCK_SIZE[1]
+        },
+        "n_bins": bins,
+        "img_dimensions": {
+            "x": shape[1],
+            "y": shape[0]
+        },
+        "resized_img_dimensions": {
+            "x": RESIZE_DIMENSIONS[0],
+            "y": RESIZE_DIMENSIONS[1]
+        },
+        "values": np.concatenate(histograms).ravel().tolist()
+    }
 
 
 def histogram_oriented_gradients(img, shape):
@@ -99,7 +331,7 @@ def histogram_oriented_gradients(img, shape):
 
             features.append(normalized_hogs.flatten())
 
-    data = {
+    return {
         "cell_dimensions": {
             "x": CELL_SIZE[0],
             "y": CELL_SIZE[1]
@@ -120,8 +352,6 @@ def histogram_oriented_gradients(img, shape):
         "values": np.concatenate(features).ravel().tolist()
     }
 
-    return data
-
 
 if len(sys.argv) < 2:
     print("Article's ID from MongoDB must be provided!")
@@ -141,23 +371,20 @@ article = articles_collection.find_one({"_id": article_id})
 images = article['images']
 
 for image in images:
-    print("Processing", image)
-
     url = image['url']
     if len(url) <= 0:
         continue
+
+    print("Processing", url)
 
     response = urllib.request.urlopen(url)
     npData = np.asarray(bytearray(response.read()), dtype="uint8")
     image_data = cv.imdecode(npData, cv.IMREAD_GRAYSCALE)
 
-    print("Image read, generate features")
-
     hog_data = histogram_oriented_gradients(image_data, image_data.shape)
-    # TODO
-    lbp_data = []
-    lbp_u_data = []
-    lbp_d_data = []
+    lbp_data = calculate_lbp(image_data, image_data.shape)
+    lbp_u_data = calculate_lbp_uniform(image_data, image_data.shape)
+    lbp_d_data = calculate_lbp_distance(image_data, image_data.shape, DISTANCE)
 
     articles_collection.update_one({
         "_id": article_id,
