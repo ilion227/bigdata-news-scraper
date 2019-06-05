b34d58dcd3de9226c4715f1e31aaaf00b1c2ee7e
ilion227 <janliber1@gmail.com>
2019-06-05 19:14:19 +0200

    Add helpers for easier data display.

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -18,4 +18,31 @@ module.exports = {
 
 		return correlationClass;
 	},
+	getClassForChi: function(value) {
+		let correlationClass = "";
+
+		if(value >= 0 && value < 4) correlationClass = "bg-translucent-success";
+		else if(value >= 4 && value < 50) correlationClass = "bg-translucent-warning";
+		else correlationClass = "bg-translucent-danger";
+
+		return correlationClass;
+	},
+	getClassForIntersect: function(value) {
+		let correlationClass = "";
+
+		if(value >= 2.5) correlationClass = "bg-translucent-success";
+		else if(value >= 2 && value < 2.5) correlationClass = "bg-translucent-warning";
+		else correlationClass = "bg-translucent-danger";
+
+		return correlationClass;
+	},
+	getClassForBhat: function(value) {
+		let correlationClass = "";
+
+		if(value >= 0 && value < 0.2) correlationClass = "bg-translucent-success";
+		else if(value >= 0.2 && value < 0.35) correlationClass = "bg-translucent-warning";
+		else correlationClass = "bg-translucent-danger";
+
+		return correlationClass;
+	},
 };
